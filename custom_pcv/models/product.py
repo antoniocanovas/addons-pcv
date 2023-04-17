a6b3cc2b3001fdd9e487d4a9946eed417ae9673d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-04-18 01:36:28 +0200

    custom_pcv

--- custom_pcv/models/product.py
+++ custom_pcv/models/product.py
@@ -9,10 +9,10 @@ class ProductTemplate(models.Model):
 
     tipo_calculo      = fields.Selection(['time','Time'], store=True, string='Tipo cálculo')
 
-    horas_minimo      = fields.float('Horas mínimas')
-    inicio_extra      = fields.float('Hora incio extra')
-    inicio_ordinario  = fields.float('Hora incio ordinaria')
-    final_ordinario   = fields.float('Hora final ordinaria')
-    final_extra       = fields.float('Hora final extra')
-    hextra_factor     = fields.float('Factor extra')
+    horas_minimo      = fields.Float('Horas mínimas')
+    inicio_extra      = fields.Float('Hora incio extra')
+    inicio_ordinario  = fields.Float('Hora incio ordinaria')
+    final_ordinario   = fields.Float('Hora final ordinaria')
+    final_extra       = fields.Float('Hora final extra')
+    hextra_factor     = fields.Float('Factor extra')
 #    hfestivo_factor   = fields.float('Factor festivo')
