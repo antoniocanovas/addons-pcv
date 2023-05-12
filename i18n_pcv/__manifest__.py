d67a695a70f515cfa8fb47bf632c228e6af30643
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-05-12 16:54:05 +0200

    cc v0

---
+++ i18n_pcv/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Serincloud SL.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    'name': 'PCV Translation',
+    'version': '16.0.1.0.0',
+    'author': 'Serincloud SL',
+    'website': 'https://github.com/antoniocanovas/addons-pcv',
+    'license': 'LGPL-3',
+    'category': 'Website',
+    'summary': 'PCV custom translations.',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
