a688af6a50b2989f2e6c4a578b3cc9263f0dc925
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-05-15 15:32:05 +0200

    event, stand, pol

--- custom_pcv/models/purchase_order_line.py
+++ custom_pcv/models/purchase_order_line.py
@@ -10,18 +10,23 @@ class PurchaseOrderLine(models.Model):
     def _get_sale_event(self):
         for record in self:
             record.event_id = record.sale_order_id.event_id.id
-    event_id        = fields.Many2one('event.event', string='Event',
+    event_id = fields.Many2one('event.event', string='Event',
                                       domain=[('is_finished','=',False),('is_published','=',True)],
-                                      compute=_get_sale_event)
+#                                      compute='_get_sale_event'
+                               )
 
     @api.depends('create_date')
     def _get_sale_stand_name(self):
         for record in self:
             record.event_id = record.sale_order_id.stand_name
-    stand_name      = fields.Char('Stand name', store=True, compute=_get_sale_stand_name)
+    stand_name      = fields.Char('Stand name', store=True,
+#                                  compute='_get_sale_stand_name'
+                                  )
 
     @api.depends('create_date')
     def _get_sale_stand_number(self):
         for record in self:
             record.event_id = record.sale_order_id.stand_number
-    stand_number    = fields.Char('Stand number', store=True, compute=_get_sale_stand_number)
+    stand_number    = fields.Char('Stand number', store=True,
+#                                  compute='_get_sale_stand_number'
+                                  )
