43a4f8dcb8094cf243d63f56ecea9bccf2418600
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-26 10:33:22 +0200

    purchase sales

--- custom_pcv/models/purchase_order.py
+++ custom_pcv/models/purchase_order.py
@@ -17,6 +17,28 @@ class PurchaseOrder(models.Model):
     sale_order_ids = fields.Many2many(comodel_name='sale.order', string='Sale orders', store=True,
                                       compute='_get_sale_orders_ids')
 
+    @api.depends('sale_order_ids')
+    def _get_events_name(self):
+        for record in self:
+            event_names = []
+            for li in record.order_line:
+                event = li.sale_line_id.order_id.event_id
+                if (event.id) and (event.name not in event_names):
+                    event_names.append(event.name)
+            record['event_name'] = str(event_names)
+    event_name = fields.Char('Event', store=False, compute='_get_events_name')
+
+    @api.depends('sale_order_ids')
+    def _get_stand_numbers(self):
+        for record in self:
+            stand_numbers = []
+            for li in record.order_line:
+                event = li.sale_line_id.order_id.event_id
+                if (event.id) and (event.stand_number not in stand_numbers):
+                    stand_numbers.append(event.stand_number)
+            record['stand_number'] = str(stand_numbers)
+    stand_number = fields.Char('Stand', store=False, compute='_get_stand_numbers')
+
     # Función para confirmar automáticamente los pedidos que vienen del comercio electrónico (ver acción automática):
     def website_sale_purchase_auto_confirm(self):
         for record in self:
