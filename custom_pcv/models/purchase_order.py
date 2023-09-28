4485fc0f3f1434e9a62f53d8ac723321a271297f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2023-09-28 09:37:36 +0200

    purchase sales

--- custom_pcv/models/purchase_order.py
+++ custom_pcv/models/purchase_order.py
@@ -37,9 +37,20 @@ class PurchaseOrder(models.Model):
                 event = li.sale_line_id.order_id.event_id
                 if (event.id) and (li.sale_line_id.order_id.stand_number not in stand_numbers):
                     stand_numbers.append(li.sale_line_id.order_id.stand_number)
-                    names += str(li.sale_line_id.order_id.stand_number) + " "
+                    names += str(li.sale_line_id.order_id.stand_number) + " " + str(li.sale_line_id.order_id.stand_name) + ". "
             record['stand_number'] = names
     stand_number = fields.Char('Stand', store=False, compute='_get_stand_numbers')
+
+    # PCV ha hecho el pedido de venta, va a ser siempre el mismo según el cliente así que m2o al primero:
+    @api.depends('sale_order_ids')
+    def _get_sale_customer(self):
+        for record in self:
+            sale_customer = False
+            for li in record.order_line:
+                if (li.order_partner_id.id) and (sale_customer == False):
+                    sale_customer = li.order_partner_id.id
+            record['sale_partner_id'] = sale_customer.id
+    sale_partner_id = fields.Many2one('res.partner', string='Customer', store=False, compute='_get_sale_customer')
 
     # Función para confirmar automáticamente los pedidos que vienen del comercio electrónico (ver acción automática):
     def website_sale_purchase_auto_confirm(self):
